dc951d004b2968da6d4624ad3aea6a7f9bc2587c
Miki <38962249+ikim333@users.noreply.github.com>
2020-05-03 17:42:09 +0200

    Comentaris i netejar Part C + error partC

--- PartC_06.m
+++ PartC_06.m
@@ -3,7 +3,7 @@
 %AUTHORS: Miquel Badia, Daniel Longaron, Aranu Reyes
 %TEAM 06
 
-%Code description
+%This program solves the incompressible NS equations 
 
 %%
 clear all
@@ -17,77 +17,80 @@ L = 1;    %Lenght of the domain (square domain, Lenght = Width)
 visc = 1.2; 
 density = 1; % density [kg/m^3]
 
-N = 10;   % number mesh divisions
+N = [5 10 15 20];   % number mesh divisions
 nts = 60; % number of time steps
 
-delta = L/N;
+% Initialize vectors where the values of velocity, pressure and kinetic
+% energy for node (3,3), for each instant of time analized, will be stored
+u_num = zeros(1,nts); v_num = zeros(1,nts); K_num = zeros(1,nts); P_num = zeros(1,nts);
+u_an = zeros(1,nts); v_an = zeros(1,nts); K_an = zeros(1,nts); P_an = zeros(1,nts);
 
-% Now we can set up the necessary vectors to plot the results later
-u_num = zeros(1,nts);
-v_num = zeros(1,nts);
-K_num = zeros(1,nts);
-P_num = zeros(1,nts);
-u_an = zeros(1,nts);
-v_an = zeros(1,nts);
-K_an = zeros(1,nts);
-P_an = zeros(1,nts);
+%Initialize time and error vectors
 t = zeros(1,nts);
+error = zeros (1, length (N));
 
-%% ANALITIC SOLUTION
-% Now we calculate the analytical solution for the velocities and pressure
-% fields when t = 0s
+%% PROCESSING
 
-[u_an0,v_an0,P_an0] = analytic_solution_C (N, delta, t, visc, density);
-
-k = kinetic_energy(u_an0, v_an0, delta, N);
-
-%We call the velocity numerical field function calculator
-[u,v] = arbitrary_velocities_C(N, delta, visc,t(1));
-
-[p, u1, v1, div_u] = pressure_field (u, v, N, delta, visc, density);
-
-K = kinetic_energy(u1, v1, delta, N);
-
-u_num(1)=u1(3,3);
-v_num(1)=v1(3,3);
-P_num(1)=p(3,3);
-K_num(1)=K;
-u_an(1)=u_an0(3,3);
-v_an(1)=v_an0(3,3);
-P_an(1)=P_an0(3,3);
-K_an(1)=K;
-
-% Process iteration for each time step
-for i=2:nts
+for n=1:length(N) 
     
-    delta_t = time_step(u, v, delta, visc); %[s]
-    t(i) = t(i-1) + delta_t; %[s]
+    delta = L/N(n);
     
-    [u, v] = arbitrary_velocities_C (N, delta, visc, t(i));
-    
-    [p, u1, v1, div_u] = pressure_field (u, v, N, delta, visc, density);
+    %% CALCUALTIONS FOR T=0
+    % Now we calculate the analytical solution for the velocities and pressure
+    % fields when t = 0s
+    [u_an0,v_an0,P_an0] = analytic_solution_C (N(n), delta, t, visc, density);
 
-    K = kinetic_energy(u1, v1, delta, N);
+    k = kinetic_energy(u_an0, v_an0, delta, N(n));
+
+    %Calculate analitic velocity values
+    [u,v] = arbitrary_velocities_C(N(n), delta, visc,t(1));
+
+    %Calculate numeric pressure and velocity field
+    [p, u1, v1, div_u] = pressure_field (u, v, N(n), delta, visc, density);
+
+    %Calculate the kinetic energy
+    K = kinetic_energy(u1, v1, delta, N(n));
+
     
-    [u_an0, v_an0, P_an0] = analytic_solution_C (N, delta, t(i), visc, density);
-    k = kinetic_energy(u_an0,v_an0,delta,N);
-    
-    %Finally we fill the desired vectors for post-processing
-    u_num(i)=u1(3,3);
-    v_num(i)=v1(3,3);
-    P_num(i)=p(3,3);
-    K_num(i)=K;
-    u_an(i)=u_an0(3,3);
-    v_an(i)=v_an0(3,3);
-    P_an(i)=P_an0(3,3);
-    K_an(i)=K;
-    
+    %Save the numerical and analitycal values in node (3, 3)for the initial
+    %time step
+    u_num(1)=u1(3,3);    v_num(1)=v1(3,3);    P_num(1)=p(3,3);    K_num(1)=K;
+    u_an(1)=u_an0(3,3);    v_an(1)=v_an0(3,3);    P_an(1)=P_an0(3,3);    K_an(1)=K;
+
+    %% ITERATION FOR EACH TIME STEP
+    for i=2:nts
+
+        delta_t = time_step(u, v, delta, visc); %[s]
+        t(i) = t(i-1) + delta_t; %[s]
+        
+        %Calculate analitic velocity values
+        [u, v] = arbitrary_velocities_C (N(n), delta, visc, t(i));
+
+        %Calculate numeric pressure and velocity field
+        [p, u1, v1, div_u] = pressure_field (u, v, N(n), delta, visc, density);
+
+        %Calculate the kinetic energy
+        K = kinetic_energy(u1, v1, delta, N(n));
+
+        [u_an0, v_an0, P_an0] = analytic_solution_C (N(n), delta, t(i), visc, density);
+        k = kinetic_energy(u_an0,v_an0,delta,N(n));
+
+        error(n) = vel_error_C(u, v, u1, v1, error(n));
+        %Save the numerical and analitycal values in node (3, 3)for the
+        %currentn time step
+        u_num(i)=u1(3,3);        v_num(i)=v1(3,3);        P_num(i)=p(3,3);        K_num(i)=K;
+        u_an(i)=u_an0(3,3);        v_an(i)=v_an0(3,3);        P_an(i)=P_an0(3,3);        K_an(i)=K;
+
+    end
 end
-
 %% POSTPROCESSING
 
-%Plot velocity
+%Plot error
 
+
+
+
+%Plot velocity
 figure
 hold on
 grid on
